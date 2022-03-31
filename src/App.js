03752a1e5dc807a28f25e0e14274744de7d790e6
Niels de Jong <niels-121@hotmail.com>
2022-03-31 16:06:31 +0200

    Integrated viz library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import GraphVisualization from 'react-graph-viz-engine'
+
+export const App = () => {
+  return <GraphVisualization
+    data={{
+      data: {
+        actors: [
+          {
+            ID: 1,
+            __typename: 'Actor',
+            acted_in: [
+              {
+                ID: 3,
+                __typename: 'Movie',
+                genres: [
+                  {
+                    ID: 5,
+                    __typename: 'Genre',
+                    name: 'Action'
+                  },
+                  {
+                    ID: 6,
+                    __typename: 'Genre',
+                    name: 'Adventure'
+                  }
+                ],
+                title: 'Trip to the Moon, A (Voyage dans la lune, Le)'
+              }
+            ],
+            name: 'François Lallement'
+          },
+          {
+            ID: 2,
+            __typename: 'Actor',
+            acted_in: [
+              {
+                ID: 3,
+                __typename: 'Movie',
+                genres: [
+                  {
+                    ID: 7,
+                    __typename: 'Genre',
+                    name: 'Sci-Fi'
+                  }
+                ],
+                title: 'Trip to the Moon, A (Voyage dans la lune, Le)'
+              }
+            ],
+            name: 'Jules-Eugène Legris'
+          }
+        ]
+      }
+    }}
+    layout="graph"
+    renderer="cytoscape"
+    showNavigator
+  />;
 }
 
 export default App;
